a4fdaf29b267a550e4d1965de8cd217f01b82dc1
bharatchandran <bharatchandrankr@gmail.com>
2023-09-05 10:52:00 +0530

    login/add notes/ view all notes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,13 +8,11 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
+    <nav className='flex justify-center' >
+<div className='w-[50%] flex justify-between'>      
       <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      <Link to="" onClick={handleLogOut}>Log Out</Link>
+      </div>
     </nav>
   );
 }
